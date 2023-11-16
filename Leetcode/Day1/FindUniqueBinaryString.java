53efdf7da24022598f387d19000930e91e7255a0
Ravi Mistry <mistryravi224@gmail.com>
2023-11-16 14:00:54 +0530

    solved

---
+++ Leetcode/Day1/FindUniqueBinaryString.java
@@ -0,0 +1,36 @@
+package Leetcode.Day1;
+//1980
+//16-11-23
+
+import java.util.Arrays;
+
+public class FindUniqueBinaryString {
+     public static String findDifferentBinaryString(String[] nums) {
+        
+        int n = nums.length;
+        Arrays.sort(nums);
+        int ptr = 0;
+
+        for(int i=0 ; i<n ; i++){
+
+            Long decVal = Long.parseLong(nums[i],2);
+
+            if(decVal == ptr){
+                ptr++;
+            }
+            else{
+                return String.format("%"+n+"s",Long.toBinaryString(ptr)).replace(' ','0');
+            }
+        }
+
+        return String.format("%"+n+"s",Long.toBinaryString(ptr)).replace(' ','0');
+
+    }
+
+    public static void main(String[] args) {
+        String [] nums = {"111","011","001"};
+        String ans = findDifferentBinaryString(nums);
+
+        System.out.println(ans);
+    }
+}
